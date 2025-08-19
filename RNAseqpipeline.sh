6c703b02ec4203a60972588891b9b1e4ae5d2404
emoeller <luminarada80@gmail.com>
2025-08-19 13:54:21 -0400

    Finalized the RNAseqpipeline.sh script with output directory structure, logging, and settings. Created a run_multiple_samples.sh script to submit jobs for multiple samples

--- RNAseqpipeline.sh
+++ RNAseqpipeline.sh
@@ -1,119 +1,135 @@
-#! /bin/bash
+#!/bin/bash -l
+
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH -c 8
+#SBATCH --mem=32G
+#SBATCH --time=02:00:00
 
 set -euo pipefail
 
 source activate bioinformatics
 module load STAR/2.7.3a
 module load fastqc
+module load cufflinks/2.2.1
 module load samtools/1.13
 module load subread/2.0.6
 
-cd /gpfs/Labs/Uzun/SCRIPTS/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA/elcheva_bulk_rna_analysis
-
-SAMPLE_NAME="K562_shCNTL_Rep1"
+BASE=/gpfs/Labs/Uzun/SCRIPTS/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA/elcheva_bulk_rna_analysis
+cd $BASE
 
 DATA_DIR="/gpfs/Labs/Uzun/DATA/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA"
 FASTQ_DATA_DIR="${DATA_DIR}/FASTQ_FILES/RAW/BATCH_2025_01"
 
-TEST_FILE_R1="${FASTQ_DATA_DIR}/${SAMPLE_NAME}_R1.fastq.gz"
-TEST_FILE_R2="${FASTQ_DATA_DIR}/${SAMPLE_NAME}_R2.fastq.gz"
+TEST_FILE_R1="${FASTQ_DATA_DIR}/${SAMPLE_NAME}_R1.fastq.gz" # Only use R1
 
-RESULTS_DIR="/gpfs/Labs/Uzun/RESULTS/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA"
+RESULTS_DIR="/gpfs/Labs/Uzun/RESULTS/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA/${SAMPLE_NAME}" #
+OUTPUT_DIR="${RESULTS_DIR}/read_alignment"
+mkdir -p $OUTPUT_DIR
 
 # ----- Genome Annotation Files -----
 genome_index_dir=/gpfs/Labs/Uzun/DATA/GENOMES/INDEX/HUMAN/HG38/STAR
 gene_annot_gtf=/gpfs/Labs/Uzun/DATA/GENOMES/ANNOTATION/HUMAN/HG38/GENE_ANNOT/gencode.v38.annotation.gtf
 gene_annot_bed=/gpfs/Labs/Uzun/DATA/GENOMES/ANNOTATION/HUMAN/HG38/GENE_ANNOT/gencode.v38.annotation.gtf.bed
 
-# --- Trimmomatic paths ---
-BASE=/gpfs/Labs/Uzun/SCRIPTS/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA/elcheva_bulk_rna_analysis
-TRIMMO_JAR="$BASE/trimmomatic-0.39/dist/jar/trimmomatic-0.39.jar"
-ADAPTERS="$BASE/trimmomatic-0.39/adapters/TruSeq3-PE.fa"
 
 # ----- Other Dataset Paths -----
 METADATA_DIR="/gpfs/Labs/Uzun/METADATA/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA"
 COMPARISON_FILE="${METADATA_DIR}/Comparisons.2025_01.txt"
 SAMPLE_ANNOT_FILE="${METADATA_DIR}/Sample_Annotation.2025_01.txt"
 
+TRIMMO_JAR="$BASE/trimmomatic-0.39/dist/jar/trimmomatic-0.39.jar"
+ADAPTERS="$BASE/trimmomatic-0.39/adapters/TruSeq3-SE.fa"
+
 SECONDS=0
 
+
+# This pipeline runs QC and alignment for reads generated using Lexogen's QuantSeq 3' mRNA-Seq
+# V2 Library Prep Kit Forward with 12 nucleotide Unique Dual Indices (UDIs).
+
+# Notes:
+#  1. Only R1 reads are used, as R2 reads start with poly(T) and are low quality
+
+echo "Output Parent Directory: ${OUTPUT_DIR}"
+
 # Run FastQC
-# echo "Running FastQC read quality assessment, saving to ${RESULTS_DIR}/initial_fastqc_reports"
-# mkdir -p "${RESULTS_DIR}/initial_fastqc_reports/${SAMPLE_NAME}"
-# fastqc $TEST_FILE_R1 -o "${RESULTS_DIR}/initial_fastqc_reports/${SAMPLE_NAME}"
+echo "Running FastQC read quality assessment"
+echo "    - Saving to Output Directory/01_raw_read_fastqc"
+mkdir -p "${OUTPUT_DIR}/01_raw_read_fastqc"
+fastqc $TEST_FILE_R1 -o "${OUTPUT_DIR}/01_raw_read_fastqc"
 
-# Adapter Trimming using trimmomatic
-# echo "Trimming adapter sequences"
-# mkdir -p ${RESULTS_DIR}/trimmed_reads
-# java -jar trimmomatic-0.39/dist/jar/trimmomatic-0.39.jar PE -threads 8 -phred33 \
-#     $TEST_FILE_R1 \
-#     $TEST_FILE_R2 \
-#     $RESULTS_DIR/trimmed_reads/${SAMPLE_NAME}_R1_trimmed.fastq \
-#     $RESULTS_DIR/trimmed_reads/${SAMPLE_NAME}_R2_trimmed.fastq \
-#     ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
-#     LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
-
-# echo "Trimmomatic finished running!"
+# Adapter trimming using trimmomatic
+echo "Trimming adapters and low-quality reads using Trimmomatic"
+echo "    - Saving to Output Directory/02_trimmomatic"
+mkdir -p "${OUTPUT_DIR}/02_trimmomatic"
+java -jar $TRIMMO_JAR SE -threads 8 -phred33 \
+    $TEST_FILE_R1 ${OUTPUT_DIR}/02_trimmomatic/${SAMPLE_NAME}_R1_trimmed.fastq.gz \
+    ILLUMINACLIP:$ADAPTERS:2:30:10 \
+    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:20
 
 # Run FastQC on the trimmed reads
-# echo "Running FastQC on the trimmed reads"
-# mkdir -p "${RESULTS_DIR}/trimming_fastqc_reports"
-# fastqc $RESULTS_DIR/trimmed_reads/${SAMPLE_NAME}_R1_trimmed.fastq -o $RESULTS_DIR/trimming_fastqc_reports
+echo "Running FastQC on the trimmed reads"
+echo "    - Saving to Output Directory/03_trimmed_read_fastqc"
+mkdir -p "${OUTPUT_DIR}/03_trimmed_read_fastqc"
+fastqc ${OUTPUT_DIR}/02_trimmomatic/${SAMPLE_NAME}_R1_trimmed.fastq.gz -o ${OUTPUT_DIR}/03_trimmed_read_fastqc
 
 # Mapping reads to the genome using STAR
-# echo "Running STAR for alignment"
-# mkdir -p $RESULTS_DIR/STAR_alignment/${SAMPLE_NAME}/
-# STAR --genomeDir "$genome_index_dir" \
-#      --runThreadN 8 \
-#      --readFilesIn "$RESULTS_DIR/trimmed_reads/${SAMPLE_NAME}_R1_trimmed.fastq" "$RESULTS_DIR/trimmed_reads/${SAMPLE_NAME}_R2_trimmed.fastq" \
-#      --readFilesCommand zcat \
-#      --twopassMode Basic \
-#      --outFileNamePrefix "$RESULTS_DIR/STAR_alignment/${SAMPLE_NAME}/${SAMPLE_NAME}_" \
-#      --outSAMtype BAM SortedByCoordinate \
-#      --limitBAMsortRAM 16000000000 \
-#      --outSJfilterReads Unique \
-#      --outSAMattributes Standard \
-#      --outSAMattrRGline ID:$SAMPLE_NAME SM:$SAMPLE_NAME PL:ILLUMINA
+echo "Running STAR for alignment"
+echo "    - Saving to Output Directory/04_star_alignment"
+mkdir -p ${OUTPUT_DIR}/04_star_alignment
+STAR --genomeDir "$genome_index_dir" \
+     --runThreadN 8 \
+     --readFilesIn "${OUTPUT_DIR}/02_trimmomatic/${SAMPLE_NAME}_R1_trimmed.fastq.gz" \
+     --readFilesCommand zcat \
+     --twopassMode Basic \
+     --outFileNamePrefix "${OUTPUT_DIR}/04_star_alignment/${SAMPLE_NAME}_" \
+     --outSAMtype BAM SortedByCoordinate \
+     --limitBAMsortRAM 16000000000 \
+     --outSJfilterReads Unique \
+     --outSAMattributes Standard \
+     --outSAMattrRGline ID:$SAMPLE_NAME SM:$SAMPLE_NAME PL:ILLUMINA
 
-# echo "STAR finished running!"
+echo "      STAR finished running!"
 
 # Build a BAM index
 echo "Building BAM index"
-module load samtools/1.13
-samtools index ${RESULTS_DIR}/STAR_alignment/${SAMPLE_NAME}/${SAMPLE_NAME}_Aligned.sortedByCoord.out.bam
+samtools index ${OUTPUT_DIR}/04_star_alignment/${SAMPLE_NAME}_Aligned.sortedByCoord.out.bam
 
 # Filter unique reads
 echo "Filtering unique reads"
-mkdir -p ${RESULTS_DIR}/TEMP_SORTING/test/
-samtools view -bh -q 255 ${RESULTS_DIR}/STAR_alignment/${SAMPLE_NAME}/${SAMPLE_NAME}_Aligned.sortedByCoord.out.bam \
-    > ${RESULTS_DIR}/STAR_alignment/${SAMPLE_NAME}/${SAMPLE_NAME}_Aligned.bam
+echo "    - Saving to Output Directory/04_star_alignment/${SAMPLE_NAME}_Aligned.bam"
+samtools view -bh -q 255 ${OUTPUT_DIR}/04_star_alignment/${SAMPLE_NAME}_Aligned.sortedByCoord.out.bam \
+    > ${OUTPUT_DIR}/04_star_alignment/${SAMPLE_NAME}_Aligned.bam
 
 # Compute FPKM with Cufflinks
 echo "Running Cufflinks for FPKM"
-module load cufflinks/2.2.1
-mkdir -p ${RESULTS_DIR}/cufflinks_fpkm_results/
-cufflinks-2.2.1.Linux_x86_64/cufflinks -p 8 \
-    --library-type fr-firststrand \
-    -o ${RESULTS_DIR}/cufflinks_fpkm_results/ \
-    -G $gene_annot_gtf \
-    ${RESULTS_DIR}/STAR_alignment/${SAMPLE_NAME}/${SAMPLE_NAME}_Aligned.bam
+echo "    - Saving to Output Directory/05_cufflinks_fpkm"
 
-echo "FPKM computed"
+mkdir -p ${OUTPUT_DIR}/05_cufflinks_fpkm
+cufflinks -p 8 \
+    --library-type fr-secondstrand \
+    -o ${OUTPUT_DIR}/05_cufflinks_fpkm/ \
+    -G $gene_annot_gtf \
+    ${OUTPUT_DIR}/04_star_alignment/${SAMPLE_NAME}_Aligned.bam
+
+echo "      FPKM computed"
 
 # Compute integer read counts with featureCounts
 echo "Running featureCounts for read counts"
-mkdir -p "${RESULTS_DIR}/featurecounts/${SAMPLE_NAME}"
+echo "    - Saving to Output Directory/06_featureCounts"
+
+mkdir -p "${OUTPUT_DIR}/06_featureCounts"
 featureCounts \
     -T 8 \
-    -p \
-    -B \
-    -C \
-    -s 2 \
+    -s 1 \
     -a "$gene_annot_gtf" \
     -t exon \
     -g gene_id \
-    -o "${RESULTS_DIR}/featurecounts/${SAMPLE_NAME}/${SAMPLE_NAME}_gene_counts.txt" \
-    "${RESULTS_DIR}/STAR_alignment/${SAMPLE_NAME}/${SAMPLE_NAME}_Aligned.bam"
+    -o "${OUTPUT_DIR}/06_featureCounts/${SAMPLE_NAME}_gene_counts.txt" \
+    "${OUTPUT_DIR}/04_star_alignment/${SAMPLE_NAME}_Aligned.bam"
+
+mkdir -p ${BASE}/gene_counts
+mv "${OUTPUT_DIR}/06_featureCounts/${SAMPLE_NAME}_gene_counts.txt" "${RESULTS_DIR}/gene_counts/${SAMPLE_NAME}_gene_counts.txt"
 
 echo "featureCounts done."
 
---
+++ run_multiple_samples.sh
@@ -0,0 +1,53 @@
+MAX_JOBS_IN_QUEUE=5
+
+BASE_DIR="/gpfs/Labs/Uzun/SCRIPTS/GRANT_APPS/2025.NYNRIN.DOD.ELCHEVA/elcheva_bulk_rna_analysis"
+
+SAMPLE_NAMES=(
+    # "K562_shCNTL_Rep1"
+    "K562_shCNTL_Rep2"
+    "K562_shCNTL_Rep3"
+
+    "K562_shNYN3_Rep1"
+    "K562_shNYN3_Rep2"
+    "K562_shNYN3_Rep3"
+
+    "K562_shNYN4_Rep1"
+    "K562_shNYN4_Rep2"
+    "K562_shNYN4_Rep3"
+
+    "KG1A_shCNTL_Rep1_S18"
+    "KG1A_shCNTL_Rep2_S20"
+    "KG1A_shCNTL_Rep3_S22"
+
+    "KG1A_shNYN4_Rep1_S19"
+    "KG1A_shNYN4_Rep2_S21"
+    "KG1A_shNYN4_Rep3_S23"
+
+    "SKNAS_shCNTL_Rep1"
+    "SKNAS_shCNTL_Rep2"
+    "SKNAS_shCNTL_Rep3"
+
+    "SKNAS_shNYN4_Rep1"
+    "SKNAS_shNYN4_Rep2"
+    "SKNAS_shNYN4_Rep3"
+)
+
+for SAMPLE_NAME in "${SAMPLE_NAMES[@]}"; do
+    while [ "$(squeue -u "$USER" -h -t PD,R -n 'RNAseqpipeline_*' -o '%i' | wc -l)" -ge "$MAX_JOBS_IN_QUEUE" ]; do
+        echo "[INFO] Max jobs ($MAX_JOBS_IN_QUEUE) running/pending. Sleeping 60s..."
+        sleep 60
+    done
+
+    LOGDIR=${BASE_DIR}/LOGS/${SAMPLE_NAME}
+    mkdir -p $LOGDIR
+
+    sbatch \
+        --export=ALL,SAMPLE_NAME="$SAMPLE_NAME" \
+        --job-name="RNAseqpipeline_${SAMPLE_NAME}" \
+        --output="${LOGDIR}/${SAMPLE_NAME}.log" \
+        --error="${LOGDIR}/${SAMPLE_NAME}.err" \
+        "${BASE_DIR}/RNAseqpipeline.sh"
+
+    sleep 2
+
+done
